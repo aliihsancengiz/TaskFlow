b09a29fb61c5bbbe27bd4124668107891dc7c0bc
Ali ihsan Cengiz <ali.ihsan.cengiz@profitap.com>
2022-11-04 21:42:00 +0100

    Add Example

---
+++ example/ExampleTasks.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <iostream>
+#include <functional>
+#include <memory>
+#include "WorkerGroup.hpp"
+#include "TaskListener.hpp"
+#include <mutex>
+
+std::mutex m;
+
+struct UploadTask : taskBase
+{
+	UploadTask(std::string url, std::string filename) : _url(url), _filename(filename) {}
+	void upload_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "Upload " << _filename << " to " << _url << std::endl;
+	}
+
+private:
+	std::string _url, _filename;
+};
+
+struct DownloadTask : taskBase
+{
+	DownloadTask(std::string url, std::string filename) : _url(url), _filename(filename) {}
+	void download_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "Downloding file to " << _filename << " from " << _url << std::endl;
+	}
+
+private:
+	std::string _url, _filename;
+};
+
+struct DeviceFwUploadTaskListener : TaskListenerBase
+{
+	DeviceFwUploadTaskListener(TaskChannel &ch) : TaskListenerBase(ch)
+	{
+		ch.registerTask<UploadTask>(&DeviceFwUploadTaskListener::handle_upload_task, this);
+		ch.registerTask<DownloadTask>(&DeviceFwUploadTaskListener::handle_download_task, this);
+	}
+	void handle_upload_task(std::shared_ptr<UploadTask> &upInst)
+	{
+		upInst->upload_task();
+	}
+	void handle_download_task(std::shared_ptr<DownloadTask> &downInst)
+	{
+		downInst->download_task();
+	}
+};
+
+struct OpenedEvent : taskBase
+{
+	void open_event_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "on Opened Event" << std::endl;
+	}
+};
+
+struct ClosedEvent : taskBase
+{
+	void close_event_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "on Closed Event" << std::endl;
+	}
+};
+
+struct DataReceivedEvent : taskBase
+{
+	void data_rcv_event_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "on DataReceived Event" << std::endl;
+	}
+};
+
+struct DataTransmittedEvent : taskBase
+{
+	void data_transmitted_event_task()
+	{
+		std::lock_guard<std::mutex> g(m);
+		std::cout << "on DataTransmitted Event" << std::endl;
+	}
+};
+
+
---
+++ example/main.cpp
@@ -0,0 +1,67 @@
+#include "ExampleTasks.hpp"
+
+struct MyDeviceTaskListener : TaskListenerBase
+{
+	MyDeviceTaskListener(TaskChannel &ch) : TaskListenerBase(ch)
+	{
+		ch.registerTask<UploadTask>(&MyDeviceTaskListener::handle_upload_task, this);
+		ch.registerTask<DownloadTask>(&MyDeviceTaskListener::handle_download_task, this);
+	}
+	void handle_upload_task(std::shared_ptr<UploadTask> &upInst)
+	{
+		upInst->upload_task();
+	}
+	void handle_download_task(std::shared_ptr<DownloadTask> &downInst)
+	{
+		downInst->download_task();
+	}
+};
+
+
+struct DeviceEventListener : TaskListenerBase
+{
+	DeviceEventListener(TaskChannel &ch) : TaskListenerBase(ch)
+	{
+		ch.registerTask<OpenedEvent>(&DeviceEventListener::handle_open, this);
+		ch.registerTask<ClosedEvent>(&DeviceEventListener::handle_close, this);
+		ch.registerTask<DataReceivedEvent>(&DeviceEventListener::handle_data_rcv, this);
+		ch.registerTask<DataTransmittedEvent>(&DeviceEventListener::handle_data_transmit, this);
+	}
+	void handle_open(std::shared_ptr<OpenedEvent> &openInst)
+	{
+		openInst->open_event_task();
+	}
+
+	void handle_close(std::shared_ptr<ClosedEvent> &openInst)
+	{
+		openInst->close_event_task();
+	}
+
+	void handle_data_rcv(std::shared_ptr<DataReceivedEvent> &openInst)
+	{
+		openInst->data_rcv_event_task();
+	}
+
+	void handle_data_transmit(std::shared_ptr<DataTransmittedEvent> &openInst)
+	{
+		openInst->data_transmitted_event_task();
+	}
+};
+
+int main()
+{
+	DefaultSchedulerPolicy policy;
+	WorkerGroup wg(policy);
+	TaskChannel ch(wg);
+	DeviceFwUploadTaskListener listener(ch);
+	DeviceEventListener evListener(ch);
+
+	listener.get_channel().push(std::make_shared<UploadTask>("my_server_url:/upload/image/", "firmware.bin"));
+	listener.get_channel().push(std::make_shared<DownloadTask>("my_server_url:/download/image/", "firmware.bin"));
+	evListener.get_channel().push(std::make_shared<OpenedEvent>());
+	evListener.get_channel().push(std::make_shared<ClosedEvent>());
+	evListener.get_channel().push(std::make_shared<DataReceivedEvent>());
+	evListener.get_channel().push(std::make_shared<DataTransmittedEvent>());
+
+	return 0;
+}
