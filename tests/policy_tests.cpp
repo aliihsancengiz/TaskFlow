f3906799fad911e3150b8ce14f7ed2707ea4c6d2
Ali ihsan Cengiz <34631659+aliihsancengiz@users.noreply.github.com>
2022-11-11 20:05:21 +0000

    Add policy tests (#18)

---
+++ tests/policy_tests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "SchedulePolicy.hpp"
+
+TEST(SchedulePolicy, RoundRobinTest)
+{
+	DefaultSchedulerPolicy policy;
+	std::vector<size_t> mListOfWorkerIds;
+	const int wIdSize = 4;
+	for (size_t i = 0; i < wIdSize; i++)
+	{
+		mListOfWorkerIds.push_back(i);
+	}
+	policy.set_worker_id_list(mListOfWorkerIds);
+
+	for (size_t i = 0; i < wIdSize * 12; i++)
+	{
+		EXPECT_EQ(policy.get_next_id(), i % wIdSize);
+	}
+}
+
+TEST(SchedulePolicy, BoundOne)
+{
+	const int boundedTaskId = 3;
+	BoundOnePolicy policy(boundedTaskId);
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		EXPECT_EQ(policy.get_next_id(), boundedTaskId);
+	}
+}
